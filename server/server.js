ab7bf0418eeec82bb03962934b12791a00d8210e
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-07-25 00:16:52 +0530

    made basic express server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/",(req,res,next)=>{
+    console.log("server started!");
+    res.send('<h1>hello</h1>');
+});
+
+app.listen(3000);
