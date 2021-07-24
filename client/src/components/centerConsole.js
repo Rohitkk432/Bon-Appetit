ec81c8547b6af446caff4996a238ff88f2a7e044
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-07-24 23:39:38 +0530

    inited server and added basic panels on client layout

---
+++ client/src/components/centerConsole.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./centerConsole.css";
+
+class CenterConsole extends Component{
+    render(){
+        return(
+            <>
+                <div className="centerConsole">
+
+                </div>
+            </>
+        )
+    }
+}
+
+export default CenterConsole;
---
+++ client/src/components/sideTab.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./sideTab.css";
+
+class SideTab extends Component{
+    render(){
+        return(
+            <>
+                <div className="sideTab">
+
+                </div>
+            </>
+        )
+    }
+}
+
+export default SideTab;
