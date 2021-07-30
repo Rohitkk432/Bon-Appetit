30bd8c3f73174942559033d6ee903b7ab3762493
Rohit Kodam <74586376+Rohitkk432@users.noreply.github.com>
2021-07-30 22:35:22 +0530

    trying to not get server down as soon as deployed

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// const pool = require('./pool');
+const pool = require('./pool');
 const routes = require('./routes/routes');
 
 //middleware
@@ -17,6 +17,15 @@ const Port = process.env.PORT || 5000 ;
 //     user : 'postgres',
 //     password : 'rohitkk432'
 // });
+
+pool.connect({
+    host : 'ec2-54-211-160-34.compute-1.amazonaws.com',
+    port : '5432',
+    database : 'df695m07nmo83q',
+    user : 'ntwikrlwevxrdv',
+    password : '4a4176ce8e2d27b6ffe3e2f2e1b85d14038841e892c147a24900f720ecb03d51'
+});
+
 
 app.use('/',routes);
 
